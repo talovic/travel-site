3c0d3ba4d4c121636e5e2d4094eae1661046840e
Talovic <dzevad88@gmail.com>
2017-09-11 13:54:21 +0200

    added mixins, wrapper, page section styles etc.

--- app/gulp/tasks/styles.js
+++ app/gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars,nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars,nested, autoprefixer]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
