06db0f7ceefd026f21e63e3600da372504c3c44e
Talovic <dzevad88@gmail.com>
2017-09-24 19:39:32 +0200

    lazy loading fix for custom waypoints

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,9 +1,10 @@
-import $ from 'jquery';
+ import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor(){
+    this.lazyImages = $(".lazyload");
     this.siteHeader = $('.site-header');
     this.headerTriggerElement = $(".large-hero__title");
     this.createHeaderWaypoint();
@@ -11,7 +12,15 @@ class StickyHeader {
     this.headerLinks = $(".primary-nav a");
     this.createPageSectionWaypoints();
     this.addSmoothScrolling();
+    this.refreshWaypoints();
   }
+
+  refreshWaypoints() {
+    this.lazyImages.load(function () {
+      Waypoint.refreshAll();
+    });
+  }
+
 
   addSmoothScrolling(){
     this.headerLinks.smoothScroll();
