54afd540187d98ea877f846233a35c53edd9046c
Talovic <dzevad88@gmail.com>
2017-09-28 19:39:25 +0200

    Files prepared to go live

--- app/gulp/tasks/build.js
+++ app/gulp/tasks/build.js
@@ -1,13 +1,42 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 del = require('del'),
-usemin = require('gulp-usemin');
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
+
+
+gulp.task('previewDist', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "dist"
+    }
+  });
+});
+
 
 gulp.task('deleteDistFolder', function() {
   return del("./dist");
 });
 
-gulp.task('optimizeImages', ['deleteDistFolder'], function() {
+gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
+  var pathsToCopy = [
+    './app/**/*',
+    '!./app/index.html',
+    '!./app/assets/images/**',
+    '!./app/assets/scripts/**',
+    '!./app/assets/styles/**',
+    '!./app/temp',
+    '!./app/temp/**',
+  ]
+  return gulp.src(pathsToCopy)
+  .pipe(gulp.dest("./dist"))
+});
+
+gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
   return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons','!./app/assets/images/icons/**/*'])
   .pipe(imagemin({
     progressive: true,
@@ -17,10 +46,13 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function() {
   .pipe(gulp.dest("./dist/assets/images"))
 });
 
-gulp.task('usemin', ['deleteDistFolder'], function() {
+gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
   return gulp.src("./app/index.html")
-  .pipe(usemin())
+  .pipe(usemin({
+    css: [function() {return rev()}, function() {return cssnano()}],
+    js: [function() {return rev()}, function() {return uglify()}]
+  }))
   .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('build',['deleteDistFolder', 'optimizeImages', 'usemin']);
+gulp.task('build',['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'usemin']);
